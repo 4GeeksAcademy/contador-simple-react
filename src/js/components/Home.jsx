4cefe45002228a1fe6e11bb92ae10afd16603bcd
Edwin González <130111056+edslogia@users.noreply.github.com>
2025-05-07 14:44:59 +0000

    alerta de tiempo cumplido funcionando

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,37 +1,57 @@
 import Contador from "./Contador";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "../../styles/home.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faClock } from "@fortawesome/free-regular-svg-icons";
-import { faRotateRight, faPlay, faPause } from "@fortawesome/free-solid-svg-icons";
+import {
+  faRotateRight,
+  faPlay,
+  faPause,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Home = () => {
   const [time, setTime] = useState(0);
   const [countdownTime, setCountdownTime] = useState(0);
+  const [alertTime, setAlertTime] = useState(0);
   const [counterActive, setCounterActive] = useState(false);
 
+  const timeRef = useRef(time);
+
+  useEffect(() => {
+    timeRef.current = time;
+  }, [time]);
+
   const handlePlay = () => {
-      setCounterActive(true);
+    setCounterActive(true);
   };
 
   const handlePause = () => {
     setCounterActive(false);
-};
+  };
 
-const handleReset = () => {
-  setCounterActive(false);
-  setTime(0);
-};
+  const handleReset = () => {
+    setCounterActive(false);
+    setTime(0);
+  };
+
+  const handleAlert = () => {
+    setAlertTime();
+  };
 
   useEffect(() => {
     const interval = setInterval(() => {
       if (counterActive) {
-        setTime((prevTime) => prevTime + 1);        
+        if (alertTime > 0 && timeRef.current === alertTime) {
+          setCounterActive(false);
+          alert("El tiempo se ha cumplido");
+          setTime((prevTime) => prevTime - 1);
+        }
+        setTime((prevTime) => prevTime + 1);
       }
     }, 500);
 
     return () => clearInterval(interval);
-  }, [counterActive]);
+  }, [counterActive, alertTime]);
 
   const unidades = time % 10;
   const decenas = Math.floor((time / 10) % 10);
@@ -76,14 +96,9 @@ const handleReset = () => {
               className="form-control"
               type="number"
               aria-label="With input"
+              value={alertTime}
+              onChange={(e) => setAlertTime(parseInt(e.target.value))}
             />
-            <button
-              className="btn btn-outline-secondary"
-              type="button"
-              id="button-alert"
-            >
-              Generar alerta
-            </button>
           </div>
           <div className="form-text" id="basic-addon4">
             Ingresa un tiempo para generar alerta
